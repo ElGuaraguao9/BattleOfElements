1227d06976cf40c3ef88c35c8375fa220c3bb989
Emanuel <emdero91@outlook.com>
2024-07-06 15:02:52 -0400

    add enemy random pet

--- JS/script1.js
+++ JS/script1.js
@@ -5,20 +5,38 @@ function startGame() {
   let buttonPetPlayer = document.getElementById("button-pets");
   buttonPetPlayer.addEventListener("click", selectPetPlayer);
 }
-//Function to select pet, if not alert user to do so
+//Function to selected pet, if not alert user to do so
 function selectPetPlayer() {
   let intputHipodoge = document.getElementById("Hipodoge");
   let intputCapipepo = document.getElementById("Capipepo");
   let intputRatigueya = document.getElementById("Ratigueya");
-  let spanPetPlayer = document.getElementById('petUser')
+  let spanPetPlayer = document.getElementById("petUser");
 
   if (intputHipodoge.checked) {
-    spanPetPlayer.innerHTML = 'Hipodoge'
+    spanPetPlayer.innerHTML = "Hipodoge";
   } else if (intputCapipepo.checked) {
-    spanPetPlayer.innerHTML = 'Capipepo'
+    spanPetPlayer.innerHTML = "Capipepo";
   } else if (intputRatigueya.checked) {
-    spanPetPlayer.innerHTML = 'Ratigueya'
+    spanPetPlayer.innerHTML = "Ratigueya";
   } else {
     alert("Please select a Pet");
+  }
+  selectPetEnemy();
+}
+//this will give a random number
+function randomE(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+//Enemy will receive a random pet
+function selectPetEnemy() {
+  let enemyAttack = randomE(1, 3);
+  let spanPetEnemy = document.getElementById("petEnemy");
+
+  if (enemyAttack == 1) {
+    spanPetEnemy.innerHTML = "Hipodoge";
+  } else if (enemyAttack == 2) {
+    spanPetEnemy.innerHTML = "Capipepo";
+  } else {
+    spanPetEnemy.innerHTML = "Ratigueya";
   }
 }
